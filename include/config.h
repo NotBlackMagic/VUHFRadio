7fb029d71c9095687dd31008e2b8975aea131fb8
NotBlackMagic <fabian_naef@live.com>
2022-09-10 18:18:14 +0100

    -Hash and version update

--- include/config.h
+++ include/config.h
@@ -6,20 +6,20 @@ extern "C" {
 #endif
 
 //Hardware Identification Defines
-#define HARDWARE_NAME						"VUHFRadio"
+#define HARDWARE_NAME						"VUHFRadio M.2"
 #define HARDWARE_VERSION_MAJOR				1
-#define HARDWARE_VERSION_MINOR				3
+#define HARDWARE_VERSION_MINOR				0
 #define HARDWARE_HASH
 
 //Software Identification Defines
 #define SOFTWARE_VERSION_MAJOR				1
-#define SOFTWARE_VERSION_MINOR				6
-#define SOFTWARE_HASH						0x0569b685
+#define SOFTWARE_VERSION_MINOR				7
+#define SOFTWARE_HASH						0x059332a4
 
 //CAT Interface
 #define CAT_VERSION_MAJOR					1
-#define CAT_VERSION_MINOR					6
-#define CAT_VERSION_HASH					0x0569b685
+#define CAT_VERSION_MINOR					7
+#define CAT_VERSION_HASH					0x059332a4
 
 #ifdef __cplusplus
 }
